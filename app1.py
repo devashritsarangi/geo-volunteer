4f126b37c9f4510eff00b45ba8a44e35c6468fb6
Devashrit Sarangi <sarangidevashrit@gmail.com>
2026-04-16 01:31:08 +0530

    rebased and added 1st appch

---
+++ app1.py
@@ -0,0 +1,33 @@
+# Approach 1: BRUTE FORCE METHOD
+# This involves having one user at a certain coordinate wrt origin, and various volunteers at coordinates wrt origin.
+# This program scours through every single volunteer in determining the closest possible volunteer to client.
+# Very time consuming if we consider in a global scale, but if we limit our bounding area to just a single city it still is very inefficient but gets the job done.
+
+# Good for simplicity, bad for scalability.
+
+volunteers = [
+    {"name": "tanmay", "x": 40, "y": 5},
+    {"name": "swastik", "x": 20, "y": 3},
+    {"name": "aditya rout", "x": 100, "y": 4}
+]
+client = {"name": "devashrit", "x": 40, "y": 10}
+distances = []
+
+print(f"Devashrit is at: {client["x"]}, {client["y"]}")
+print("Running through all volunteers...")
+
+def sqr_dist(client, volunteer):
+    x = volunteer["x"] - client["x"]
+    y = volunteer["y"] - client["y"]
+    return (x**2) + (y**2)
+
+for volunteer in volunteers:
+    dist = sqr_dist(client, volunteer)
+    distances.append({"name": volunteer["name"], "distance": dist})
+
+distances.sort(key=lambda item: item["distance"])
+
+# 5. Print the 1st, 2nd, and 3rd closest
+print("1st closest:", distances[0]["name"])
+print("2nd closest:", distances[1]["name"])
+print("3rd closest:", distances[2]["name"])
